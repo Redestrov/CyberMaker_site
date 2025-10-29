535904250ba2bfb906d7ec416f8effd26eb87992
Felipe de Castro <felipedecastroalves074@gmail.com>
2025-10-29 18:20:35 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const app = express();
 // Health route for Railway
 app.get("/", (req, res) => res.send("OK"));
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" })); // permite até 10 megabytes
+
 app.use(cors());
 app.use(cors());
 app.use(express.json({ limit: "10mb" })); // allow image uploads as base64
